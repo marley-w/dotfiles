353cf25220db01ecaec0f669c1163d853877cdb2
marley-w <marleymrw@proton.me>
2024-12-04 04:48:46 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo pacman -S lib32-pipewire \
   vulkan-validation-layers lib32-vulkan-validation-layers vulkan-headers \
   vulkan-extra-layers mesa lib32-mesa vulkan-mesa-layers lib32-vulkan-mesa-layers \
   vulkan-radeon lib32-vulkan-radeon libva lib32-libva libva-mesa-driver \
-  lib32-libva-mesa-driver mesa-utils linux-zen-headers lib32-pipewire gtk-layer-shell
+  lib32-libva-mesa-driver mesa-utils linux-zen-headers gtk-layer-shell
 
 # Applications
 sudo pacman -S steam wine ttf-meslo-nerd neovim kitty
