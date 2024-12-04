c68536dd827c2dc2cb94308e354b15bb07c7d4c1
marley-w <marleymrw@proton.me>
2024-12-04 04:47:34 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ sudo pacman -S lib32-pipewire \
   vulkan-validation-layers lib32-vulkan-validation-layers vulkan-headers \
   vulkan-extra-layers mesa lib32-mesa vulkan-mesa-layers lib32-vulkan-mesa-layers \
   vulkan-radeon lib32-vulkan-radeon libva lib32-libva libva-mesa-driver \
-  lib32-libva-mesa-driver mesa-utils
+  lib32-libva-mesa-driver mesa-utils linux-zen-headers lib32-pipewire gtk-layer-shell
 
 # Applications
-sudo pacman -S steam wine ttf-meslo-nerd firefox neovim kitty
+sudo pacman -S steam wine ttf-meslo-nerd neovim kitty
 
 # Desktop
 sudo pacman -S hyprland xdg-desktop-portal-hyprland nwg-look \
@@ -18,5 +18,5 @@ sudo pacman -S hyprland xdg-desktop-portal-hyprland nwg-look \
 
 # Utilities
 sudo pacman -S bluez bluez-utils gamemode lib32-gamemode mangohud lib32-mangohud \
-  gstreamer lib32-gstreamer npm dart-sass go wofi cpupower
+  gstreamer lib32-gstreamer modemmanager git cpupower
 
